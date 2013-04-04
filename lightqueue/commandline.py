7092ea134159c60e5db6d0e781fa21572d8b84e0
Adam Phan <aphansh@gmail.com>
2013-04-04 14:30:39 -0700

    Updated setup

--- lightqueue/commandline.py
+++ lightqueue/commandline.py
@@ -2,45 +2,50 @@ import argparse
 from lightqueue.factory import Factory
 
 
-parser = argparse.ArgumentParser()
+def start():
+    parser = argparse.ArgumentParser()
 
-parser.add_argument('command')
+    parser.add_argument('command')
 
-parser.add_argument('-e', action='store', dest='execution',
-                    default='sequential', help='The execution model')
+    parser.add_argument('-e', action='store', dest='execution',
+                        default='sequential',
+                        help='execution model (sequential or parallel)')
 
-parser.add_argument('-workers', action='store', dest='workers', type=int,
-                    default=4, help='The execution model')
+    parser.add_argument('-workers', action='store', dest='workers', type=int,
+                        default=4,
+                        help='# of jobs processed at a time (for -e parallel)')
 
-parser.add_argument('-host', action='store', dest='host',
-                    default='localhost', help='The redis server\'s hostname')
+    parser.add_argument('-host', action='store', dest='host',
+                        default='localhost',
+                        help='redis server\'s hostname')
 
-parser.add_argument('-port', action='store', dest='port', type=int,
-                    default=6379, help='The redis server\'s port')
+    parser.add_argument('-port', action='store', dest='port', type=int,
+                        default=6379, help='redis server\'s port')
 
-parser.add_argument('-db', action='store', dest='db', type=int,
-                    default=0, help='The redis server\'s db')
+    parser.add_argument('-db', action='store', dest='db', type=int,
+                        default=0, help='redis server\'s db')
 
-parser.add_argument('-qname', action='store', dest='queue_name',
-                    default='lightqueue', help='The execution model')
+    parser.add_argument('-qname', action='store', dest='queue_name',
+                        default='lightqueue',
+                        help='lightqueue namespace in Redis db')
 
-parser.add_argument('--version', action='version', version='%(prog)s 0.1.0')
+    parser.add_argument('--version', action='version',
+                        version='%(prog)s 0.1.0')
 
+    # Parse the command
+    results = parser.parse_args()
 
-# Parse the command
-results = parser.parse_args()
+    if results.command == 'start':
 
-if results.command == 'start':
+        multiprocessing = False
+        if results.execution == 'parallel':
+            multiprocessing = True
 
-    multiprocessing = False
-    if results.execution == 'parallel':
-        multiprocessing = True
-
-    Factory(multiprocessing=multiprocessing,
-            workers=results.workers,
-            host=results.host,
-            port=results.port,
-            db=results.db,
-            queue_name=results.queue_name).start()
-else:
-    print 'Use "lightqueue start" to start processing jobs'
+        Factory(multiprocessing=multiprocessing,
+                workers=results.workers,
+                host=results.host,
+                port=results.port,
+                db=results.db,
+                queue_name=results.queue_name).start()
+    else:
+        print 'Use "lightqueue start" to start processing jobs'
--- setup.py
+++ setup.py
@@ -13,9 +13,16 @@ setup(
     long_description=open('README.txt').read(),
     entry_points={
         'console_scripts': [
-            'lightqueue = lightqueue.commandline',
+            'lightqueue = lightqueue.commandline:start',
         ]
     },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+    ],
     install_requires=[
         "redis >= 2.7.2",
     ],
